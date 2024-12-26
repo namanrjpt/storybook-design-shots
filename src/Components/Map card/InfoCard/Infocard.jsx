eb118c432c618244446b49a6c9af0e3f51c23804
namanrjpt <mail.namanrajput@gmail.com>
2024-12-26 20:34:11 +0530

    Updated Map Card

--- src/Components/Map card/InfoCard/Infocard.jsx
+++ src/Components/Map card/InfoCard/Infocard.jsx
@@ -1,7 +1,110 @@
 import React from 'react';
 
-const Infocard = () => {
-	return <div></div>;
+const Infocard = ({ data, type }) => {
+	const Issuecard = () => {
+		return (
+			<div className='bg-green-100 rounded-lg w-[20rem] min-h-[10rem] overflow-hidden'>
+				<div className='flex items-start flex-col justify-start p-4 bg-green-100'>
+					<p className='text-black text-base font-bold'>
+						{data['issue']}
+					</p>
+					<p className='text-sm font-regular text-black/75'>
+						{data['description']}
+					</p>
+				</div>
+				<div className='flex w-full flex-col justify-center p-3 py-4 gap-3 rounded-t-xl bg-white'>
+					<div className='w-full flex justify-between'>
+						<p className='text-sm'>Operator</p>
+						<div className='flex items-center justify-center gap-2'>
+							{data.isResolverAssigned && (
+								<div className='flex items-center justify-center bg-red-600 w-6 h-6 aspect-square rounded-full text-white'>
+									{data.resolver['operator'][0]}
+								</div>
+							)}
+							<p className='font-semibold text-sm text-black'>
+								{data.isResolverAssigned
+									? data.resolver['operator']
+									: 'Not Assigned'}
+							</p>
+						</div>
+					</div>
+					<div className='w-full flex justify-between'>
+						<p className='text-sm'>Arrival Time</p>
+						<p className='font-semibold text-sm text-black'>
+							{data.isResolverAssigned
+								? data.resolver['Arrival Time']
+								: '-'}
+						</p>
+					</div>
+					<div className='w-full flex justify-between'>
+						<p className='text-sm'>Vehicle</p>
+						<p className='font-semibold text-sm text-black'>
+							{data.isResolverAssigned
+								? data.resolver['Vehicle']
+								: '-'}
+						</p>
+					</div>
+					<div className='w-full flex justify-between'>
+						<p className='text-sm'>Vehicle No</p>
+						<p className='font-semibold text-sm text-black'>
+							{data.isResolverAssigned
+								? data.resolver['Vehicle No']
+								: '-'}
+						</p>
+					</div>
+				</div>
+			</div>
+		);
+	};
+	const Resolvercard = () => {
+		return (
+			<div className='bg-green-100 rounded-lg w-[20rem] min-h-[10rem] overflow-hidden'>
+				<div className='flex items-center justify-start p-4 bg-green-100'>
+					<div className='h-10 rounded-full flex items-center justify-center bg-red-600 aspect-square font-bold text-white'>
+						{data.resolver['operator'][0]}
+					</div>
+					<div className='w-full flex justify-center items-start flex-col pl-4'>
+						<p className='text-black text-base font-bold'>
+							{data.resolver['operator']}
+						</p>
+						<p className='text-xs font-regular text-black/75'>
+							Updated : 2 mins ago
+						</p>
+					</div>
+				</div>
+				<div className='flex w-full flex-col justify-center p-3 py-4 gap-3 bg-white rounded-t-xl'>
+					<div className='w-full flex justify-between'>
+						<p className='text-sm'>Vehicle</p>
+						<p className='font-semibold text-sm text-black'>
+							Black Toyota Hilux
+						</p>
+					</div>
+					<div className='w-full flex justify-between'>
+						<p className='text-sm'>Vehicle No</p>
+						<p className='font-semibold text-sm text-black'>
+							BDSI SMR
+						</p>
+					</div>
+					<div className='w-full flex justify-between'>
+						<p className='text-sm'>Last job</p>
+						<p className='font-semibold text-sm text-black'>
+							MTC000173
+						</p>
+					</div>
+				</div>
+			</div>
+		);
+	};
+
+	return (
+		<>
+			{type === 'issue' ? (
+				<Issuecard data={data} />
+			) : (
+				<Resolvercard data={data} />
+			)}
+		</>
+	);
 };
 
 export default Infocard;
--- src/Components/Map card/Map/Map.jsx
+++ src/Components/Map card/Map/Map.jsx
@@ -16,13 +16,22 @@ import 'leaflet-routing-machine/dist/leaflet-routing-machine.css';
 import { RiSendPlaneFill } from 'react-icons/ri';
 import { FaRegCircleDot } from 'react-icons/fa6';
 import { motion } from 'framer-motion';
+import InfoCard from '../InfoCard/InfoCard';
+import { FiSearch } from 'react-icons/fi';
+import { FiFilter } from 'react-icons/fi';
+import { HiOutlineChevronDown } from 'react-icons/hi2';
+import './Map.css';
 
 const Map = () => {
-	const position = [51.505, -0.09]; // Issue position
+	const position = [
+		51.50446014671324, -0.08978672794743331,
+	]; // Issue position
 	const [data, setData] = useState([
 		{
 			id: 1,
 			issue: 'Issue 1',
+			description:
+				'Toilet handle in public restroom is broken and needs to be replaced',
 			position: [51.506963, -0.092569], // Issue position
 			isResolverAssigned: true,
 			showRoute: false,
@@ -41,6 +50,7 @@ const Map = () => {
 		{
 			id: 2,
 			issue: 'Issue 2',
+			description: 'Street light is not working',
 			position: [51.504367, -0.094288], // Issue position
 			isResolverAssigned: true,
 			showRoute: false,
@@ -59,6 +69,7 @@ const Map = () => {
 		{
 			id: 3,
 			issue: 'Issue 3',
+			description: 'Pothole on the road',
 			position: [51.504346, -0.082057], // Issue position
 			isResolverAssigned: true,
 			showRoute: false,
@@ -77,13 +88,14 @@ const Map = () => {
 		{
 			id: 4,
 			issue: 'Issue 4',
+			description: 'Garbage bin is overflowing',
 			position: [51.506568, -0.096743], // Issue position
 			isResolverAssigned: false,
 			showRoute: false,
 			resolver: {
 				resolverId: 'MTC001200',
 				position: [51.512, -0.097], // Resolver position
-				color: 'yellow',
+				color: 'orange',
 				operator: 'Bob Brown',
 				description:
 					'Bob Brown is a very experienced resolver',
@@ -93,6 +105,14 @@ const Map = () => {
 			},
 		},
 	]);
+
+	const [showRoutes, setShowRoutes] = useState({});
+
+	const toggleRoute = (id) => {
+		setShowRoutes((prev) => ({
+			[id]: !prev[id],
+		}));
+	};
 
 	const issueIcon = new L.DivIcon({
 		className: 'leaflet-div-icon1',
@@ -104,66 +124,85 @@ const Map = () => {
 		iconAnchor: [15, 15],
 	});
 
-	const RoutingMachine = ({ waypoints }) => {
-		const map = useMap();
+	const RoutingMachine = React.memo(
+		({ waypoints }) => {
+			const map = useMap();
 
-		useEffect(() => {
-			if (!map) return;
+			useEffect(() => {
+				if (!map) return;
 
-			const routingControl = L.Routing.control({
-				waypoints: waypoints.map((point) =>
-					L.latLng(point)
-				),
-				routeWhileDragging: true,
-				createMarker: () => null,
-				show: false,
-				lineOptions: {
-					styles: [
-						{
-							color: '#0088ff',
-							weight: 3,
-							opacity: 1,
-							dashArray: '8, 8',
-						},
-					],
-				},
-				fitSelectedRoutes: false,
-				showAlternatives: false,
-				addWaypoints: false,
-				draggableWaypoints: false,
-			}).addTo(map);
+				const routingControl = L.Routing.control({
+					waypoints: waypoints.map((point) =>
+						L.latLng(point)
+					),
+					routeWhileDragging: true,
+					createMarker: () => null,
+					show: false,
+					lineOptions: {
+						styles: [
+							{
+								color: '#0088ff',
+								weight: 3,
+								opacity: 1,
+								dashArray: '8, 8',
+							},
+						],
+					},
+					fitSelectedRoutes: false,
+					showAlternatives: false,
+					addWaypoints: false,
+					draggableWaypoints: false,
+				}).addTo(map);
 
-			// Hide routing container
-			const style = document.createElement('style');
-			style.textContent =
-				'.leaflet-routing-container { display: none !important; }';
-			document.head.appendChild(style);
+				// Hide routing container
+				const style = document.createElement('style');
+				style.textContent =
+					'.leaflet-routing-container { display: none !important; }';
+				document.head.appendChild(style);
 
-			return () => {
-				map.removeControl(routingControl);
-				document.head.removeChild(style);
-			};
-		}, [map, waypoints]);
+				return () => {
+					map.removeControl(routingControl);
+					document.head.removeChild(style);
+				};
+			}, [map, waypoints]);
 
-		return null;
-	};
+			return null;
+		}
+	);
 
 	const calculateRotationAngle = (start, end) => {
 		const dx = end[1] - start[1];
 		const dy = end[0] - start[0];
 		const angle =
-			Math.atan2(dy, dx) * (180 / Math.PI); // Convert radians to degrees
-		return angle; // This gives the angle of rotation
+			Math.atan2(dy, dx) * (180 / Math.PI);
+		return angle;
 	};
 
 	return (
 		<div
-			className='h-full w-full'
+			className='h-full w-full relative'
 			id='map'
 		>
+			<div className='absolute top-4 left-[4rem] z-[10000] w-10 h-10 flex items-center gap-2'>
+				<div className='w-auto p-2 flex items-center justify-center bg-white rounded-md shadow-lg gap-4'>
+					<FiSearch className='text-black text-xl' />
+					<input
+						type='text'
+						placeholder='Search'
+						className='outline-none w-[10rem]'
+					/>
+				</div>
+				<div className='w-auto p-2 px-4 flex items-center justify-center bg-white rounded-md gap-2 shadow-lg'>
+					<p>Jobs</p>
+					<HiOutlineChevronDown className='text-black text-xl' />
+				</div>
+				<div className='w-auto p-2 flex items-center justify-center bg-white rounded-md shadow-lg'>
+					<FiFilter className='text-black text-xl' />
+				</div>
+			</div>
 			<MapContainer
 				center={position}
-				zoom={18}
+				zoom={16}
 				scrollWheelZoom={true}
 				style={{ height: '100%', width: '100%' }}
 			>
@@ -179,50 +218,14 @@ const Map = () => {
 						key={item.id}
 						icon={issueIcon}
 						eventHandlers={{
-							click: () => {
-								setData((prevData) =>
-									prevData.map((dataItem) =>
-										dataItem.id === item.id
-											? {
-													...dataItem,
-													showRoute: !dataItem.showRoute,
-											  }
-											: dataItem
-									)
-								);
-							},
+							click: () => toggleRoute(item.id),
 						}}
 					>
 						<Popup>
-							<div className='flex flex-col'>
-								<div className='flex items-center justify-between'>
-									<h1 className='text-xl font-bold'>
-										{item.issue}
-									</h1>
-									<motion.div
-										initial={{ scale: 0 }}
-										animate={{ scale: 1 }}
-										exit={{ scale: 0 }}
-										transition={{ duration: 0.5 }}
-										className='bg-green-500 p-2 rounded-full'
-									>
-										<RiSendPlaneFill size={20} />
-									</motion.div>
-								</div>
-								<div className='flex items-center justify-between'>
-									<div className='flex items-center'>
-										<FaRegCircleDot size={20} />
-										<h1 className='text-lg font-bold'>
-											{item.resolver.operator}
-										</h1>
-									</div>
-									<div className='flex items-center'>
-										<h1 className='text-lg font-bold'>
-											{item.resolver['Arrival Time']}
-										</h1>
-									</div>
-								</div>
-							</div>
+							<InfoCard
+								data={item}
+								type='issue'
+							/>
 						</Popup>
 					</Marker>
 				))}
@@ -241,22 +244,22 @@ const Map = () => {
 						const rotatedResolverIcon = new L.DivIcon({
 							className: 'leaflet-div-icon-rotated',
 							html: `
-        <div
-          style="
-            transform: rotate(${rotationAngle}deg);
-            width: 30px;
-            height: 30px;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-			color: ${item.resolver.color};
-          "
-        >
-          <svg stroke="currentColor" fill="currentColor" stroke-width="4" viewBox="0 0 24 24" height="24" width="24" xmlns="http://www.w3.org/2000/svg">
-            <path d="M1.94607 9.31543C1.42353 9.14125 1.4194 8.86022 1.95682 8.68108L21.043 2.31901C21.5715 2.14285 21.8746 2.43866 21.7265 2.95694L16.2733 22.0432C16.1223 22.5716 15.8177 22.59 15.5944 22.0876L11.9999 14L17.9999 6.00005L9.99992 12L1.94607 9.31543Z"></path>
-          </svg>
-        </div>
-      `,
+        					<div
+        					  style="
+        					    transform: rotate(${rotationAngle}deg);
+        					    width: 30px;
+        					    height: 30px;
+        					    display: flex;
+        					    align-items: center;
+        					    justify-content: center;
+								color: ${item.resolver.color};
+        					  "
+        					>
+                            <svg stroke="currentColor" fill="currentColor" stroke-width="4" viewBox="0 0 24 24" height="24" width="24" xmlns="http://www.w3.org/2000/svg">
+            				<path d="M1.94607 9.31543C1.42353 9.14125 1.4194 8.86022 1.95682 8.68108L21.043 2.31901C21.5715 2.14285 21.8746 2.43866 21.7265 2.95694L16.2733 22.0432C16.1223 22.5716 15.8177 22.59 15.5944 22.0876L11.9999 14L17.9999 6.00005L9.99992 12L1.94607 9.31543Z"></path>
+          					</svg>
+        					</div>
+      						`,
 							iconSize: [30, 30],
 							iconAnchor: [15, 15], // Center of the icon
 						});
@@ -267,54 +270,16 @@ const Map = () => {
 								key={item.id}
 								icon={rotatedResolverIcon}
 								eventHandlers={{
-									click: () => {
-										setData((prevData) => {
-											const updatedData = prevData.map(
-												(dataItem) => {
-													if (dataItem.id === item.id) {
-														return {
-															...dataItem,
-															showRoute: !dataItem.showRoute,
-														};
-													}
-													return dataItem;
-												}
-											);
-											return updatedData;
-										});
-									},
+									click: () => toggleRoute(item.id),
 								}}
 							>
-								<Popup>
-									<div className='flex flex-col'>
-										<div className='flex items-center justify-between'>
-											<h1 className='text-xl font-bold'>
-												{item.issue}
-											</h1>
-											<motion.div
-												initial={{ scale: 0 }}
-												animate={{ scale: 1 }}
-												exit={{ scale: 0 }}
-												transition={{ duration: 0.5 }}
-												className='bg-green-500 p-2 rounded-full'
-											>
-												<RiSendPlaneFill size={20} />
-											</motion.div>
-										</div>
-										<div className='flex items-center justify-between'>
-											<div className='flex items-center'>
-												<FaRegCircleDot size={20} />
-												<h1 className='text-lg font-bold'>
-													{item.resolver.operator}
-												</h1>
-											</div>
-											<div className='flex items-center'>
-												<h1 className='text-lg font-bold'>
-													{item.resolver['Arrival Time']}
-												</h1>
-											</div>
-										</div>
-									</div>
+								<Popup
+									style={{ zIndex: 1000, width: 'auto' }}
+								>
+									<InfoCard
+										data={item}
+										type='Resolver'
+									/>
 								</Popup>
 							</Marker>
 						);
@@ -324,7 +289,8 @@ const Map = () => {
 				{data
 					.filter(
 						(item) =>
-							item.isResolverAssigned && item.showRoute
+							item.isResolverAssigned &&
+							showRoutes[item.id]
 					)
 					.map((item) => (
 						<RoutingMachine
