13212cb1a60b83d8a702bb0e149d99e77928474e
namanrjpt <mail.namanrajput@gmail.com>
2024-12-26 23:13:39 +0530

    Updated Map Card Story

---
+++ src/Components/Map card/Final/FinalMap.stories.jsx
@@ -0,0 +1,10 @@
+import Final from './FinalMap';
+
+export default {
+	title: 'Dribble Shots/Map Card/Final Render',
+};
+
+export const FinalMapCard = (args) => (
+	<Final {...args} />
+);
+FinalMapCard.storyName = 'Map Card';
